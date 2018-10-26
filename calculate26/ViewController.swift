80472760bd45990134421bf38f9a15516e5717f4
ciecmacs20171106578 <m15886077136@163.com>
2018-10-26 19:42:00 +0800

    Initial Commit

---
+++ calculate26/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculate26
+//
+//  Created by s20171106578 on 2018/10/26.
+//  Copyright © 2018 s20171106578. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ calculate26Tests/calculate26Tests.swift
@@ -0,0 +1,34 @@
+//
+//  calculate26Tests.swift
+//  calculate26Tests
+//
+//  Created by s20171106578 on 2018/10/26.
+//  Copyright © 2018 s20171106578. All rights reserved.
+//
+
+import XCTest
+@testable import calculate26
+
+class calculate26Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
